d3013eed5d5a0635a31817228891db039c5a90bf
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-10 14:40:39 +0700

    Video-05-List-Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <header>TS Amazona</header>
-        <main></main>
+        <main>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                className='product-image'
+                src={product.image}
+                alt={product.name}
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </main>
         <footer>All right reserved</footer>
       </div>
     </>
