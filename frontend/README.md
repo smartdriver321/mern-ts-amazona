016a94766f3d925f6b0429f50f5cdce9542b25c9
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-10 12:37:47 +0700

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+# TypeScript MERN AMAZONA
+
+## Lessons
+
+1. Introduction
+2. Install Tools
+3. Create TypeScript React App By Vite
+4. Create Git Repository
+   1. add README.md
+   2. create github account
+   3. connect vs code to github
+   4. publish repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>TS Amazona</header>
+        <main></main>
+        <footer>All right reserved</footer>
+      </div>
+    </>
+  )
+}
+
+export default App
