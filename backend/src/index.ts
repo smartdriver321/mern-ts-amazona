7e9c301ca90ab0aedbf6f01adc4f3c4302dbbbf5
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-18 20:25:58 +0700

    Video-09-Fetch-Products

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 import { sampleProducts } from './data'
 
 const app = express()
 const PORT = 4000
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+)
 
 app.get(`/api/products`, (req: Request, res: Response) => {
   res.json(sampleProducts)
