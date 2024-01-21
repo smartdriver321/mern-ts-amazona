c5a8f229c20b30d4870e7b0ed99aab22adf14240
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-21 17:28:42 +0700

    Video-28-Pay-Order-By-PayPal( not fixed yet )

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { orderRouter } from './routers/orderRouter'
 import { productRouter } from './routers/productRouter'
 import { seedRouter } from './routers/seedRouter'
 import { userRouter } from './routers/userRouter'
+import { keyRouter } from './routers/keyRouter'
 
 dotenv.config()
 
@@ -36,6 +37,7 @@ app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
 app.use('/api/seed', seedRouter)
+app.use('/api/keys', keyRouter)
 
 const PORT = 4000
 app.listen(PORT, () => {
---
+++ backend/src/routers/keyRouter.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+export const keyRouter = express.Router()
+
+// /api/keys/paypal
+keyRouter.get('paypal', (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID || 'sb' })
+})
